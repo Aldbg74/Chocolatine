5c290a1ebf1f2ed9c3e1ee6411dae26ff0daf461
Alexis <63512507+Aldbg74@users.noreply.github.com>
2024-03-16 18:24:16 +0100

    Add files via upload

---
+++ test_the_coding_style.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-DOP-200-LYN-2-1-chocolatine-alexis.drago-beltran
+** File description:
+** test_the_coding_style.c
+*/
+
+#include <unistd.h>
+
+int main(void)
+{
+    write(1, "j'ai mangé un kiwi, et maintenant j'ai un petit zizi", 53);
+    return 0;
+}
